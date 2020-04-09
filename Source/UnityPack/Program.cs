1c4e9b819ca9394203b1a3d936b15b885d70eeca
Andy Tang <andyt@playstudios.com>
2020-04-09 08:43:49 -0700

    Updated to Dotnet core project

--- Source/UnityPack/Program.cs
+++ Source/UnityPack/Program.cs
@@ -3,32 +3,32 @@ using System.Collections.Generic;
 
 namespace UnityPacker
 {
-    class PackProgram
-    {
-        static void Main(string[] args)
-        {
-            if (args.Length < 2)
-            {
-                Console.WriteLine("Usage:");
-                Console.WriteLine(
-                    "UnityPacker [Source Path] [Package Name] [Respect Meta] [Omitted Extensions] [Omitted Directories]");
-                return;
-            }
+	class PackProgram
+	{
+		static void Main(string[] args)
+		{
+			if (args.Length < 2)
+			{
+				Console.WriteLine("Usage:");
+				Console.WriteLine(
+					"UnityPacker [Source Path] [Package Name] [Respect Meta] [Omitted Extensions] [Omitted Directories]");
+				return;
+			}
 
-            var inpath = args[0];
-            var fileName = args.Length > 1 ? args[1] : "Package";
-            var rootDir = args.Length > 2 ? args[2] : "Assets/";
-            var exts = args.Length > 3 ? args[3].Split(',') : new string[0];
-            var dirs = args.Length > 4 ? args[4].Split(',') : new string[0];
+			var inpath = args[0];
+			var fileName = args.Length > 1 ? args[1] : "Package";
+			var rootDir = args.Length > 2 ? args[2] : "Assets/";
+			var exts = args.Length > 3 ? args[3].Split(',') : new string[0];
+			var dirs = args.Length > 4 ? args[4].Split(',') : new string[0];
 
-            var extensions = new List<string>(exts)
-            {
-                "meta" // always skip meta files
-            };
+			var extensions = new List<string>(exts)
+			{
+				"meta" // always skip meta files
+			};
 
-            // Create a package object from the given directory
-            var pack = Package.FromDirectory(inpath, fileName, true, extensions.ToArray(), dirs);
-            pack.GeneratePackage(rootDir);
-        }
-    }
+			// Create a package object from the given directory
+			var pack = Package.FromDirectory(inpath, fileName, true, extensions.ToArray(), dirs);
+			pack.GeneratePackage(rootDir);
+		}
+	}
 }
--- Source/UnityUnpack/Program.cs
+++ Source/UnityUnpack/Program.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 
 namespace UnityPacker
 {
-    internal class UnpackProgram
-    {
-        public static void Main(string[] args)
-        {   
-            var p = Package.FromPackage(args[0]);
-            p.GenerateFolder(args.Length > 0 ? args[1] : "");
-        }
-    }
+	internal class UnpackProgram
+	{
+		public static void Main(string[] args)
+		{   
+			var p = Package.FromPackage(args[0]);
+			p.GenerateFolder(args.Length > 0 ? args[1] : "");
+		}
+	}
 }
